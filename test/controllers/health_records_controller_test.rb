13534c9169abf7e5e8e51660a4eab5e0f620c7b7
Rediet-art <rediet20308@gmail.com>
2025-12-16 23:42:42 +0000

    Fix health records controller tests

--- test/controllers/health_records_controller_test.rb
+++ test/controllers/health_records_controller_test.rb
@@ -1,18 +1,32 @@
 require "test_helper"
 
 class HealthRecordsControllerTest < ActionDispatch::IntegrationTest
+ setup do
+    @user = users(:one)
+    sign_in @user
+  end
+
   test "should get index" do
-    get health_records_index_url
+    get health_records_url
     assert_response :success
   end
 
   test "should get new" do
-    get health_records_new_url
+    get new_health_record_url
     assert_response :success
   end
 
-  test "should get create" do
-    get health_records_create_url
-    assert_response :success
+  test "should create health record" do
+    assert_difference("HealthRecord.count", 1) do
+      post health_records_url, params: {
+        health_record: {
+          weight: 70,
+          heart_rate: 72,
+          notes: "Test record"
+        }
+      }
+    end
+
+    assert_redirected_to health_records_url
   end
 end
